27656b02313f8cead5b0873d71dac5fcbe98cb75
mahendrasinghsisodiya2003 <vssisodiya2001@gmail.com>
2025-04-11 10:29:51 +0530

    master

--- frontend/my-app/src/component/Cart.jsx
+++ frontend/my-app/src/component/Cart.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { useCart } from "./cartcontext";
+import { useCart } from "./Cartcontext";
 import { useNavigate } from "react-router-dom";
 
 const Cart = () => {
